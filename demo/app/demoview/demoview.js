1d31d4bdf1fbb01a4f04190f4a820c9a57759f22
MikeroSkywalker <puzhwei@gmail.com>
2015-08-12 22:25:57 -0400

    add new

--- demo/app/demoview/demoview.js
+++ demo/app/demoview/demoview.js
@@ -3,10 +3,17 @@
 // Declare app level module which depends on views, and components
 var demoApp = angular.module('angularDemoApp');
 
-demoApp.controller('demoController', ['$scope', function($scope) {
+demoApp.controller('demoController', ['$scope','$http', function($scope,$http) {
   	$scope.Test = "Show me as a demo";
 
   	$scope.DemoButton = function(){
-  		alert("click me");
+  		$http.get("demoview/mockdata.json")
+  		.success(function(data){
+  			$scope.data = data;
+  			 alert($scope.data[0].id);
+
+  		});
+  		//Why if I put it right here will have issue
+  		//alert($scope.data[0].id);
   	};
 }]);
