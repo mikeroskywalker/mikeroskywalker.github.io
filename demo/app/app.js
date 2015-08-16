62d00defc38c7a73f1671a2c422bae5d85617a4b
MikeroSkywalker <puzhwei@gmail.com>
2015-08-16 16:21:06 -0400

    add controller

--- demo/app/app.js
+++ demo/app/app.js
@@ -1,9 +1,11 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('angularDemoApp', ['ngRoute']).
+angular.module('angularDemoApp', ['ngRoute','angularDemoApp.controller']).
 	config(['$routeProvider', function($routeProvider) {
 
 		$routeProvider.otherwise({
-	  		templateUrl: 'demoview/demoview.html'});
+	  		templateUrl: 'demoview/demoview.html',
+	  		controller: 'demoController'
+	  	});
 	}]);
